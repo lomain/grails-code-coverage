6489b7384a01f679e849780ea9f11d67f6008004
Jeremy Vinding <jeremy.vinding@closely.com>
2016-08-02 12:35:35 -0600

    make idea stop whining about grails 1.3.5 and missing dependencies

--- test/projects/sample-plugin/SamplePluginGrailsPlugin.groovy
+++ test/projects/sample-plugin/SamplePluginGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class SamplePluginGrailsPlugin {
     // the plugin version
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.5 > *"
+    def grailsVersion = "2.4.4 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
